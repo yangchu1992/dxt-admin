b491dc44fa8ce3f6f41c69bb57ba1cf15c910eed
jqchu <408978081@qq.com>
2017-07-03 20:36:15 +0800

    first commit

---
+++ src/page/dialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dialogs">
+    <div class="dialog" v-bind:class="{ 'dialog-active': show }">
+      <div class="dialog-content">
+        <div class="close rotate">
+          <span class="iconfont icon-close" @click="close"></span>
+        </div>
+        <slot name="header"></slot>
+        <slot name="body"></slot>
+        <slot name="footer"></slot>
+      </div>
+    </div>
+    <div class="dialog-overlay"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        columns: [{
+          name: 'ss'
+        }],
+        dataList: [{
+          ss: 'ss1'
+        }]
+      }
+    },
+    methods: {
+      loadEntry: function (key) {
+        // this.$dispatch('load-entry', key)
+      },
+      deleteEntry: function (entry) {
+        // this.$dispatch('delete-entry', entry)
+      }
+    }
+  }
+</script>
---
+++ src/page/grid.vue
@@ -0,0 +1,46 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th v-for="col in columns">
+        {{ col.name}}
+      </th>
+      <th>Delete</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="(index,entry) in dataList">
+      <td v-for="col in columns">
+        <span v-if="col.isKey"><a href="javascript:void(0)" @click="loadEntry(entry[col.name])">{{ entry[col.name] }}</a></span>
+        <span v-else>{{ entry[col.name] }}</span>
+      </td>
+      <td>
+        <button class="btn-danger" @click="deleteEntry(entry)">delete</button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        columns: [{
+          name: 'ss'
+        }],
+        dataList: [{
+          ss: 'ss1'
+        }]
+      }
+    },
+    methods: {
+      loadEntry: function (key) {
+        // this.$dispatch('load-entry', key)
+      },
+      deleteEntry: function (entry) {
+        // this.$dispatch('delete-entry', entry)
+      }
+    }
+  }
+</script>
